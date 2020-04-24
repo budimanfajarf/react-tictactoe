4ce158ebb28a075bd96b1623b147a9676b45981d
budimanfajarf <budimanfajarf@gmail.com>
2020-04-25 06:23:57 +0700

    cleaning up

--- src/index.js
+++ src/index.js
@@ -62,13 +62,6 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
-    // console.log("history.length", history.length);
-    // console.log("history", history);
-    // console.log("current", current);
-    // console.log("squares", squares);
-    // console.log("calculateWinner(squares)", calculateWinner(squares));
-    // console.log("squares[i]", squares[i]);
-
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -99,8 +92,6 @@ class Game extends React.Component {
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
-      // console.log("step", step);
-      // console.log("move", move);
       const desc = move ? "Go to move #" + move : "Go to game start";
       return (
         <li key={move}>
@@ -139,7 +130,6 @@ class Game extends React.Component {
 ReactDOM.render(<Game />, document.getElementById("root"));
 
 function calculateWinner(squares) {
-  // console.log("squares", squares);
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -153,13 +143,6 @@ function calculateWinner(squares) {
 
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    // console.log(
-    //   "squares[a], squares[b], squares[c]",
-    //   squares[a],
-    //   squares[b],
-    //   squares[c]
-    // );
-
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
